2efca3c12b829f50757104960e3f727ea7c0db03
John Leavell <johnleavell@gmail.com>
2023-03-27 20:51:35 +0000

    completed first 3 months

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -4,10 +4,21 @@ class NumbersController < ApplicationController
 
     5.times do
       giraffe = rand(1...100)
-      
+
       @zebra.push(giraffe)
     end
 
-    render({ :template => "lottery_stuff/woohoo.html.erb"})
-  end 
+    render({ :template => "lottery_stuff/woohoo.html.erb" })
+  end
+
+  def loosers
+    @zebra = Array.new
+
+    5.times do
+      giraffe = rand(1...100)
+
+      @zebra.push(giraffe)
+    end
+    render({ :template => "lottery_stuff/loosers.html.erb" })
+  end
 end
